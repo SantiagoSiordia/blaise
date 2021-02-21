0617deb082ae4f15a17f654b09f63a9e074180dd
Santi <s.siordia@cyberpuerta.mx>
2021-02-21 16:10:17 -0600

    feat: bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Navigator } from "./src/navigator";
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.dark}>
-    <Navigator />
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <Navigator />
+    </ApplicationProvider>
+  </>
 );
--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import { Layout, Text } from "@ui-kitten/components";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function HomeScreen() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </Layout>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </Layout>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export const Navigator = () => (
-  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
 );
