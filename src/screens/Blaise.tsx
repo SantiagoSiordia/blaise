738feb7273f7f4304971f0fa4ce09f0527e8f91b
Santi <s.siordia@cyberpuerta.mx>
2021-06-09 21:11:41 -0500

    style: new buttons

--- src/screens/Blaise.tsx
+++ src/screens/Blaise.tsx
@@ -2,23 +2,32 @@ import React, { FC } from "react";
 import { Layout } from "@ui-kitten/components";
 import { Image, StyleSheet } from "react-native";
 import TemperatureController from "../components/TemperatureController";
+import TimeController from "../components/TimeController";
+import OnButton from "../components/OnButton";
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
   blaiseImage: {
     width: 200,
-    height: 200,
+    height: 205,
   },
 });
 
 export const Blaise: FC = () => {
   return (
     <Layout style={styles.container}>
-      <TemperatureController />
+      <OnButton />
       <Image
         source={require("../assets/Blaise.png")}
         style={styles.blaiseImage}
       />
+      <TemperatureController />
+      <TimeController />
     </Layout>
   );
 };
