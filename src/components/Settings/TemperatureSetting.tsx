48956274750c9405166701826f157fb90466b754
Santi <s.siordia@cyberpuerta.mx>
2021-06-12 13:28:35 -0500

    fix: static default temp was going through function

--- src/components/Settings/TemperatureSetting.tsx
+++ src/components/Settings/TemperatureSetting.tsx
@@ -37,7 +37,7 @@ const TemperatureSetting: FC = () => {
         />
         <Button
           title={"Actualizar"}
-          onPress={() => handleSetDefaultTemperature(20)}
+          onPress={() => handleSetDefaultTemperature(newTemperature)}
           color={lapisLazuli}
         />
       </View>
